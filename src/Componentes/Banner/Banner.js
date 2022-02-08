6aab57655159bcb4c5310af44ae1d2f80e0f4636
octaviofitz <octaviofitz@gmail.com>
2022-02-07 22:03:06 -0300

    add buttom banner, fix containers compra

--- src/Componentes/Banner/Banner.js
+++ src/Componentes/Banner/Banner.js
@@ -1,11 +1,16 @@
 import '../Banner/banner.css'
+import { Button } from 'react-bootstrap';
+
 
 const Banner = () => {
     return (
         <header id='inicio'>
             <div className='banner'>
             <h1 className='title-banner'>Bentonita</h1>
-            <h2 className='subtitle-banner'>Fina bentonita patagónica.</h2>
+            <h2 className='subtitle-banner'>Fina bentonita patagónica</h2>
+            <a href="https://api.whatsapp.com/send?phone=+541132762324&text=Hola, estoy interesado en adquirir bentonita." target='_blank' rel="noreferrer noopener"><Button variant="secondary" size="lg" className='button-compra-bentonita'>
+                 COMPRAR
+             </Button></a>
             </div>
         </header>
     );
