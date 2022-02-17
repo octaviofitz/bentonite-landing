20a7e657b03c963c5b5e385b78c84e0dbe11e098
octaviofitz <octaviofitz@gmail.com>
2022-02-17 12:21:04 -0300

    test youtube-lite package

--- src/Componentes/Secciones/CompraProductos/CompraProductos.js
+++ src/Componentes/Secciones/CompraProductos/CompraProductos.js
@@ -1,5 +1,6 @@
 import '../CompraProductos/compraProductos.css'
 import { Button } from 'react-bootstrap';
+import { ReactYouTubeLite } from 'react-youtube-lite';
 
 const CompraProductos = () => {
     return (
@@ -12,7 +13,12 @@ const CompraProductos = () => {
             <p className='compra-productos-data'>¡Contactanos!</p>
                 </div>
                 <div className='container-video-boton'>
-                <iframe id='video-rubicat' height="315" src="https://www.youtube.com/embed/JX1jSK4-eIw?controls=0" title="Rubicat Arena" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                {/* <iframe id='video-rubicat' height="315" src="https://www.youtube.com/embed/JX1jSK4-eIw?controls=0" title="Rubicat Arena" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
+                <ReactYouTubeLite 
+                url="https://www.youtube.com/embed/JX1jSK4-eIw?controls=0"
+                noCookie= {true}
+                title="Rubicat Arena"
+                />
                 <a href="https://api.whatsapp.com/send?phone=+541132762324&text=Hola, estoy interesado en adquirir productos a base de bentonita." target='_blank' rel="noreferrer noopener"><Button variant="secondary" size="lg" className='button-compra-productos'>
                  Comprar
              </Button></a>
