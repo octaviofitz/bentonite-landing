6d666efa032322815478a01899a165c600c5e905
octaviofitz <octaviofitz@gmail.com>
2025-08-06 14:54:11 -0300

    numero contacto y footer

--- src/Componentes/Footer/Footer.js
+++ src/Componentes/Footer/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
             </div>
             <div>
             <div className='footer-container-iconos'>
-                <li className='footer-iconos'> <a href="https://api.whatsapp.com/send?phone=+5492215898698&text=Hola, vengo aquí a través de la web de bentonita" target='_blank' rel="noreferrer noopener"><i className="fab fa-whatsapp"></i></a></li>
+                <li className='footer-iconos'> <a href="https://api.whatsapp.com/send?phone=+5491138069397&text=Hola, vengo aquí a través de la web de bentonita" target='_blank' rel="noreferrer noopener"><i className="fab fa-whatsapp"></i></a></li>
                 <li className='footer-iconos'><a href='http://minerpat.com.ar/inicio.html' target='_blank' rel="noreferrer noopener"><i className="fas fa-globe-americas"></i></a></li>
                 <li className='footer-iconos'><a href='https://www.google.com/maps/place/Minerales+Patagonicos+SA/@-38.9239238,-67.9654662,17z/data=!3m1!4b1!4m6!3m5!1s0x960a3097711add75:0x8a27f215744e886d!8m2!3d-38.9239238!4d-67.9654662!16s%2Fg%2F119typ5ln?entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D' target='_blank' rel="noreferrer noopener"><i className="fas fa-map-marker-alt"></i></a></li>
             </div>
